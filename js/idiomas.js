522dd7c4c7741d1ab418fdbd1604e55dcd3343fb
Franco Correa <fcorrea@sophilabs.com>
2017-05-23 10:10:13 -0300

    Traducciones

--- js/idiomas.js
+++ js/idiomas.js
@@ -3,18 +3,29 @@ var IDIOMAS = {
         nombre: 'Español',
         strings: {
             'sobre-nosotros': { html: 'Sobre Nosotros' },
+            'nuestras-redes': { html: 'Nuestras redes' },
             'contacto': { html: 'Contacto' },
             'slider-1': { attr: { src: 'img/slider/1.png' } },
-            'slider-2': { attr: { src: 'img/slider/2.png' } }
+            'slider-2': { attr: { src: 'img/slider/2.png' } },
+            'copyright': { html: '&copy; <span id="anio">2017</span> 827Labs S.R.L. – Todos los derechos reservados.' },
+            'inicio': { html: 'Inicio', attr: { title: 'Inicio' } },
+            'productos': { html: 'Productos', attr: { title: 'Productos' } },
+            'quienes-somos': { html: 'Quienes Somos', attr: { title: 'Quienes Somos' } }
+            
         }
     },
     en: {
         nombre: 'English',
         strings: {
             'sobre-nosotros': { html: 'About Us' },
+            'nuestras-redes': { html: 'Our social networks' },
             'contacto': { html: 'Contact' },
             'slider-1': { attr: { src: 'img/slider/en/1.png' } },
-            'slider-2': { attr: { src: 'img/slider/en/2.png' } }
+            'slider-2': { attr: { src: 'img/slider/en/2.png' } },
+            'copyright': { html: '&copy; <span id="anio">2017</span> 827Labs S.R.L. – All rights reserved.' },
+            'inicio': { html: 'Home', attr: { title: 'Home' } },
+            'productos': { html: 'Products', attr: { title: 'Products' } },
+            'quienes-somos': { html: 'About us', attr: { title: 'About us' } }
         }
     }
 }
--- js/traduccion.js
+++ js/traduccion.js
@@ -1,10 +1,70 @@
+var LANG_KEY = 'com.827labs.lang';
 
-$(document).on('ready', function () {
-    var keys = Object.keys(IDIOMAS);
-    for (var i = 0; i < keys.length; i++) {
-        var idioma = IDIOMAS[keys[i]];
-        $('#idioma').append('<option value="' + keys[i] + '">' + idioma.nombre + '</option>');
+function setIdiomaActual(idioma) {
+    localStorage[LANG_KEY] = idioma;
+    rellenarCampos();
+}
+
+function getIdiomaActual() {
+    if (typeof localStorage[LANG_KEY] === 'undefined') {
+        localStorage[LANG_KEY] = 'es';
     }
 
+    return IDIOMAS[localStorage[LANG_KEY]];
+}
+
+function rellenarCampos() {
+    var idioma = getIdiomaActual();
     
+    $('*[data-traduccion]').each(function () {
+        var elementTranslatableKey = $(this).attr('data-traduccion');
+        var transation = idioma.strings[elementTranslatableKey];
+        
+        if(!transation) {
+            return;
+        }
+
+        if (transation.html) {
+            $(this).html(transation.html);
+        }
+        
+        if (transation.attr) {
+            for (var propiedad in transation.attr) {
+                $(this).attr(propiedad, transation.attr[propiedad]);
+            }
+        }
+    });
+}
+
+function renderComboIdiomas() {
+    var keys = Object.keys(IDIOMAS);
+    var actual = localStorage[LANG_KEY];
+    
+    $('#idioma').html('');
+    
+    for (var i = 0; i < keys.length; i++) {
+        var idioma = IDIOMAS[keys[i]];
+        if (keys[i] === actual) {
+            $('#idioma').append('<option selected value="' + keys[i] + '">' + idioma.nombre + '</option>');
+        } else {
+            $('#idioma').append('<option value="' + keys[i] + '">' + idioma.nombre + '</option>');
+        }
+    }
+}
+
+$(document).on('ready', function () {
+    
+    renderComboIdiomas();
+    rellenarCampos();
+
+    $('#idioma').on('change', function () {
+        $('#loader').fadeIn('slow');
+        setIdiomaActual($(this).val());
+        renderComboIdiomas();
+        setTimeout(function () {
+            $("#loader").fadeOut('slow');
+        }, 300);
+    })
+
+
 });
